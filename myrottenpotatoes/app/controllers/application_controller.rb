152bc4b7c1d2bab3fb14d7f3fc26c48cf8b940e5
TaslimaSiddiquea <taslima.siddiquea@mnsu.edu>
2019-04-03 02:26:33 +0000

    Made some changes to 4.4 and 4.6

--- myrottenpotatoes/app/controllers/application_controller.rb
+++ myrottenpotatoes/app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
-# This file is app/controllers/movies_controller.rb
-class MoviesController < ApplicationController
-  def index
-    @movies = Movie.all
-  def create
-    params.require(:movie)
-    params[:movie].permit(:title,:rating,:release_date)
-    # shortcut: params.require(:movie).permit(:title,:rating,:release_date)
-    # rest of code...
-  end
-  end
+class ApplicationController < ActionController::Base
 end
--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -1,10 +1,30 @@
+class MoviesController < ApplicationController
+def index
+  @movies = Movie.all
+end 
+
+def show
+  id = params[:id] # retrieve movie ID from URI route
+  @movie = Movie.find(id) # look up movie by unique ID
+  # will render app/views/movies/show.html.haml by default
+end
+
+
 def new
   @movie = Movie.new
   # default: render 'new' template
 end 
+def create
+    params.require(:movie)
+    params[:movie].permit(:title,:rating,:release_date)
+    # shortcut: params.require(:movie).permit(:title,:rating,:release_date)
+    # rest of code...
+end
 # in movies_controller.rb
 def create
   @movie = Movie.create!(params[:movie])
   flash[:notice] = "#{@movie.title} was successfully created."
   redirect_to movies_path
+end
+
 end
